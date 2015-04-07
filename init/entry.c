2cd3a77f5242814de0bec9bf1c424a0fb9334d08
Morain <morain@morain.(none)>
2015-04-07 16:48:46 +0800

    加入中断处理机制

--- init/entry.c
+++ init/entry.c
@@ -8,15 +8,20 @@
 #include "console.h"
 #include "debug.h"
 #include "gdt.h"
+#include "idt.h"
 
 int kern_entry()
 {
 	init_debug();
 	init_gdt();
+	init_idt();
 
 	console_clear();
 	printk("Hello , Morain\n");
 	printk_color(rc_black , rc_green , "Hello , OS kernel\n");	
-	panic("test");
+	//panic("test");
+
+	asm volatile("int $0x3");
+	asm volatile("int $0x4");
 	return 0;
 }
